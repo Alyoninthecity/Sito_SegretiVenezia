821fe67173bbb7e0328d13a67cf838bb45de3bc9
Alyoninthecity <Alyoninthecity@users.noreply.github.com>
2019-12-01 18:49:04 +0100

    sa

--- index.php
+++ index.php
@@ -4,11 +4,12 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="./style.css">
   <title>Segreti di venezia</title>
 </head>
 <body>
-  <div class="">
-      <img src="./assets/img/cartinaVenezia.png" alt="cartina Venezia">
+  <div id="cartinaVenezia">
+      <img id="cartinaVenezia" src="./assets/img/cartinaVenezia.png" alt="cartina Venezia">
   </div>
 </body>
 </html>
