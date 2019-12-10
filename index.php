6b866fc430adfbc3688cb68607961d1bb9f2f965
Alyoninthecity <Alyoninthecity@users.noreply.github.com>
2019-12-10 23:54:59 +0100

    l

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php include_once("header.php") ?>
-	<?php include_once("form.php") ?>
-	<?php include_once("commenti.php") ?>
+<div></div>
+<?php include_once("form.php") ?>
+<?php include_once("commenti.php") ?>
 <?php include_once("footer.php") ?>
