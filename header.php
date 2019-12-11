0a7fc8315aee947f7a61d332817c97d3d895f63a
Alyoninthecity <Alyoninthecity@users.noreply.github.com>
2019-12-11 02:59:58 +0100

    df

--- header.php
+++ header.php
@@ -12,30 +12,30 @@
 
     <div id="nav">
         <ul>
-            <li class="active navbotton"><a href="./index.php" title="">Home</a></li>
+            <li class="active navbotton"><a href="./index.php" title="Home">Home</a></li>
             <li class="dropdown">
-                <a href="javascript:void(0)" class="dropbtn">Destinazioni</a>
+                <a href="javascript:void(0)" class="dropbtn" title="Destinazioni">Destinazioni</a>
                 <div class="dropdown-content">
-                    <a href="./StazioneVenezia.php">Stazione di Venezia</a>
-                    <a href="./BasilicataDiSanMarco.php">Basilicata di San Marco</a>
-                    <a href="./CampanileDiSanMarco.php">Campanile di San Marco</a>
-                    <a href="./CampoDellaMaddalena.php">Campo della Maddalena</a>
-                    <a href="./CasinoVenier.php">Casino Venier</a>
-                    <a href="./GhettoEbraico.php">GhettoEbraico</a>
-                    <a href="./GiardiniPapadopoli.php">Giardini Papadopoli</a>
-                    <a href="./MercatodiRialto.php">Mercato di Rialto</a>
-                    <a href="./MuseoDiStoriaNaturaleDiVenezia.php">Museo Di Storia Naturale Di Venezia</a>
-                    <a href="./PalazzoDucale.php">Palazzo Ducale</a>
-                    <a href="./PalazzoSoranzoCappello.php">Palazzo Soranzo Cappello</a>
-                    <a href="./PiazzaSanMarco.php">Piazza San Marco</a>
-                    <a href="./PonteDellaCostitutizione.php">Ponte della Costituzione</a>
-                    <a href="./PonteDiRialto.php">Ponte di Rialto</a>
-                    <a href="./PontiDeiSospiri.php">Ponte dei Sospiri</a>
-                    <a href="./ScuolaGrandeDiSanRocco.php">Scuola Grande di San Rocco</a>
-                    <a href="./TeatroLaFenice.php">Teatro La Fenice</a> </div>
+                    <a href="./StazioneVenezia.php" title="Stazione di Venezia">Stazione di Venezia</a>
+                    <a href="./BasilicataDiSanMarco.php" title="Basilicata di San Marco">Basilicata di San Marco</a>
+                    <a href="./CampanileDiSanMarco.php" title="Campanile di San Marco">Campanile di San Marco</a>
+                    <a href="./CampoDellaMaddalena.php" title="Campo della Maddalena">Campo della Maddalena</a>
+                    <a href="./CasinoVenier.php" title="Casino Venier">Casino Venier</a>
+                    <a href="./GhettoEbraico.php" title="GhettoEbraico">GhettoEbraico</a>
+                    <a href="./GiardiniPapadopoli.php" title="Giardini Papadopoli">Giardini Papadopoli</a>
+                    <a href="./MercatodiRialto.php" title="Mercato di Rialto">Mercato di Rialto</a>
+                    <a href="./MuseoDiStoriaNaturaleDiVenezia.php" title="Museo Di Storia Naturale Di Venezia">Museo Di Storia Naturale Di Venezia</a>
+                    <a href="./PalazzoDucale.php" title="Palazzo Ducale">Palazzo Ducale</a>
+                    <a href="./PalazzoSoranzoCappello.php" title="Palazzo Soranzo Cappello">Palazzo Soranzo Cappello</a>
+                    <a href="./PiazzaSanMarco.php" title="Piazza San Marco">Piazza San Marco</a>
+                    <a href="./PonteDellaCostitutizione.php" title="Ponte della Costituzione">Ponte della Costituzione</a>
+                    <a href="./PonteDiRialto.php" title="Ponte di Rialto">Ponte di Rialto</a>
+                    <a href="./PontiDeiSospiri.php" title="Ponte dei Sospiri">Ponte dei Sospiri</a>
+                    <a href="./ScuolaGrandeDiSanRocco.php" title="Scuola Grande di San Rocco">Scuola Grande di San Rocco</a>
+                    <a href="./TeatroLaFenice.php" title="Teatro La Fenice">Teatro La Fenice</a> </div>
             </li>
-            <li class="navbotton"><a href="#" title="">About Us</a></li>
-            <li class="lilogo"><a href="#" title="">MPYM</a></li>
+            <li class="navbotton"><a href="./about.php" title="About">About Us</a></li>
+            <li class="lilogo" title="MPYM">MPYM</li>
         </ul>
     </div>
 
@@ -45,7 +45,7 @@
         <div class="content">
             <h2 class="title">Segreti di Venezia</h2>
             <span class="subTitle">Crea il tuo percorso personalizzato per Venezia</span>
-            <a href="./index.php#formPercorso" class="button">Start</a></li>
+            <a href="./index.php#formPercorso" class="button" title="Start">Start</a></li>
 
         </div>
         </div>
