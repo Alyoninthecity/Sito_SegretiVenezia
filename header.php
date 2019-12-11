0695f1b9e9b5085d73acf3ea9d359dc45d149fd3
Alyoninthecity <Alyoninthecity@users.noreply.github.com>
2019-12-11 02:51:46 +0100

    i

--- header.php
+++ header.php
@@ -16,23 +16,23 @@
             <li class="dropdown">
                 <a href="javascript:void(0)" class="dropbtn">Destinazioni</a>
                 <div class="dropdown-content">
-                    <a href="./destinazioni/StazioneVenezia.php">Stazione di Venezia</a>
-                    <a href="./destinazioni/BasilicataDiSanMarco.php">Basilicata di San Marco</a>
-                    <a href="./destinazioni/CampanileDiSanMarco.php">Campanile di San Marco</a>
-                    <a href="./destinazioni/CampoDellaMaddalena.php">Campo della Maddalena</a>
-                    <a href="./destinazioni/CasinoVenier.php">Casino Venier</a>
-                    <a href="./destinazioni/GhettoEbraico.php">GhettoEbraico</a>
-                    <a href="./destinazioni/GiardiniPapadopoli.php">Giardini Papadopoli</a>
-                    <a href="./destinazioni/MercatodiRialto.php">Mercato di Rialto</a>
-                    <a href="./destinazioni/MuseoDiStoriaNaturaleDiVenezia.php">Museo Di Storia Naturale Di Venezia</a>
-                    <a href="./destinazioni/PalazzoDucale.php">Palazzo Ducale</a>
-                    <a href="./destinazioni/PalazzoSoranzoCappello.php">Palazzo Soranzo Cappello</a>
-                    <a href="./destinazioni/PiazzaSanMarco.php">Piazza San Marco</a>
-                    <a href="./destinazioni/PonteDellaCostitutizione.php">Ponte della Costituzione</a>
-                    <a href="./destinazioni/PonteDiRialto.php">Ponte di Rialto</a>
-                    <a href="./destinazioni/PontiDeiSospiri.php">Ponte dei Sospiri</a>
-                    <a href="./destinazioni/ScuolaGrandeDiSanRocco.php">Scuola Grande di San Rocco</a>
-                    <a href="./destinazioni/TeatroLaFenice.php">Teatro La Fenice</a> </div>
+                    <a href="./StazioneVenezia.php">Stazione di Venezia</a>
+                    <a href="./BasilicataDiSanMarco.php">Basilicata di San Marco</a>
+                    <a href="./CampanileDiSanMarco.php">Campanile di San Marco</a>
+                    <a href="./CampoDellaMaddalena.php">Campo della Maddalena</a>
+                    <a href="./CasinoVenier.php">Casino Venier</a>
+                    <a href="./GhettoEbraico.php">GhettoEbraico</a>
+                    <a href="./GiardiniPapadopoli.php">Giardini Papadopoli</a>
+                    <a href="./MercatodiRialto.php">Mercato di Rialto</a>
+                    <a href="./MuseoDiStoriaNaturaleDiVenezia.php">Museo Di Storia Naturale Di Venezia</a>
+                    <a href="./PalazzoDucale.php">Palazzo Ducale</a>
+                    <a href="./PalazzoSoranzoCappello.php">Palazzo Soranzo Cappello</a>
+                    <a href="./PiazzaSanMarco.php">Piazza San Marco</a>
+                    <a href="./PonteDellaCostitutizione.php">Ponte della Costituzione</a>
+                    <a href="./PonteDiRialto.php">Ponte di Rialto</a>
+                    <a href="./PontiDeiSospiri.php">Ponte dei Sospiri</a>
+                    <a href="./ScuolaGrandeDiSanRocco.php">Scuola Grande di San Rocco</a>
+                    <a href="./TeatroLaFenice.php">Teatro La Fenice</a> </div>
             </li>
             <li class="navbotton"><a href="#" title="">About Us</a></li>
             <li class="lilogo"><a href="#" title="">MPYM</a></li>
